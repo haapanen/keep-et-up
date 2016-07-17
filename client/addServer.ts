cdc9c2d8665027edd5b1c83560f8df69152aa4a4
Jussi Haapanen <haapanen.jussi@gmail.com>
2016-07-18 01:43:03 +0300

    Refactored addServer command

--- client/addServer.ts
+++ client/addServer.ts
@@ -1,100 +1,58 @@
-import * as zmq from "zmq";
 import * as uuid from "node-uuid";
 import {MessageType, Message} from "../lib/messages/message";
 import {CommandType} from "../lib/messages/commands/command";
-import * as readline from "readline";
 import {Server} from "../service/bll/server";
 import {AddServerCommand} from "../lib/messages/commands/addServerCommand";
 import {Response, ResponseStatus} from "../lib/messages/response";
+import {Client} from "./client";
 
-interface SentMessage extends Message {
-    acked: boolean;
-}
-
-export class Client {
-    private rl: readline.ReadLine;
-    private socket: zmq.Socket;
-    private sentMessages: SentMessage[] = [];
+export class AddServerCommandImplementation extends Client {
     constructor() {
-        this.socket = zmq.socket("req");
-
-        this.socket.on("message", (response) => {
-            let parsed: Response = JSON.parse(response.toString());
-
-            this.sentMessages.filter(m => m.id === (parsed as any).id).forEach(m => {
-                m.acked = true
-            });
-
-            if (parsed.status === ResponseStatus.Failure) {
-                console.log(`Could not add server: ${parsed.message}`);
+        super("tcp://localhost:42424", (response: Response) => {
+            if (response.status === ResponseStatus.Failure) {
+                console.log(`Could not add server: ${response.message}`);
             } else {
-                console.log(`${parsed.message}`);
-            }
-
-            if (!this.sentMessages.some(m => !m.acked)) {
-                this.socket.close();
-                this.rl.close();
+                console.log(`${response.message}`);
             }
         });
-
-        this.socket.connect("tcp://localhost:42424");
-
-        this.rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-
-        this.run();
     }
 
     /**
      * Handles the add server main logic
      */
-    private async run() {
-        // Read attributes from user
-        let server: Server = {
-            name: await this.input("Server name: "),
-            address: await this.input("Server address: "),
-            port: parseInt((await this.input("Server port: ")), 10),
-            user: await this.input("OS user: "),
-            customExecutable: await this.input("Custom executable path: "),
-            basepath: await this.input("Server basepath: "),
-            homepath: await this.input("Server homepath: "),
-            mod: (await this.input("Server mod: ")) as any,
-            configs: ((await this.input("Server configs separated by ,: ")) as string).split(",").forEach(c => c.trim()) as any
-        };
+    async run() {
+        try {
+            // Read attributes from user
+            let server: Server = {
+                name: await this.input("Server name: "),
+                address: await this.input("Server address: "),
+                port: parseInt((await this.input("Server port: ")), 10),
+                user: await this.input("OS user: "),
+                customExecutable: await this.input("Custom executable path: "),
+                basepath: await this.input("Server basepath: "),
+                homepath: await this.input("Server homepath: "),
+                mod: (await this.input("Server mod: ")) as any,
+                configs: ((await this.input("Server configs separated by ,: ")) as string).split(",").forEach(c => c.trim()) as any
+            };
 
-        // build the packet
-        const payload: AddServerCommand = {
-            type: CommandType.AddServer,
-            server
-        };
+            // build the packet
+            const payload: AddServerCommand = {
+                type: CommandType.AddServer,
+                server
+            };
 
-        const message: SentMessage = {
-            type: MessageType.Command,
-            id: uuid.v4(),
-            payload,
-            acked: false
-        };
+            const message: Message = {
+                type: MessageType.Command,
+                id: uuid.v4(),
+                payload
+            };
 
-        // send the packet
-        this.sentMessages.push(message);
-        this.socket.send(JSON.stringify(message, null, 4));
-    }
-
-    /**
-     * Displays message to user and reads the value
-     * @param message
-     */
-    private input(message: string): Promise<string> {
-        return new Promise<string>((resolve, reject) => {
-            try {
-                this.rl.question(message, (result) => {
-                    return resolve(result);
-                });
-            } catch (exception) {
-                return reject(exception);
-            }
-        })
+            this.send(message);
+        } catch (exception) {
+            console.error(exception);
+        }
     }
 }
 
-const client = new Client();
-
+const command = new AddServerCommandImplementation();
+command.run();
---
+++ client/client.ts
@@ -0,0 +1,77 @@
+import * as zmq from "zmq";
+import * as readline from "readline";
+import * as _ from "lodash";
+import {Message} from "../lib/messages/message";
+
+interface SentMessage extends Message {
+    acked: boolean;
+}
+
+/**
+ * Base class for client side commands
+ */
+export class Client {
+    // readline object to read user input
+    private rl: readline.ReadLine;
+    // zmq socket to sent and receive messages
+    private socket: zmq.Socket;
+    // list of sent messages
+    private sentMessages: SentMessage[];
+
+    /**
+     * @param address Address to connect to
+     * @param onMessage Response object
+     */
+    constructor(address: string, onMessage: (response: any) => void) {
+        this.socket = zmq.socket("req");
+
+        this.socket.on("message", (response) => {
+            try {
+                let parsedResponse = JSON.parse(response.toString());
+                onMessage(parsedResponse);
+
+                this.sentMessages.filter(m => m.id === parsedResponse.id).forEach(m => {
+                    m.acked = true;
+                });
+
+                // if all the messages are acked, close socket and readline interface
+                if (!this.sentMessages.some(m => !m.acked)) {
+                    this.socket.close();
+                    this.rl.close();
+                }
+
+            } catch (exception) {
+                console.error("Error: Invalid response: ", exception);
+            }
+        });
+
+        this.socket.connect(address);
+        this.rl = readline.createInterface({ input: process.stdin, output: process.stdout  });
+        this.sentMessages = [];
+    }
+
+    /**
+     * Sends a message to the endpoint
+     * @param message
+     */
+    send(message: Message) {
+        this.sentMessages.push(_.extend(message, { acked: false }));
+        this.socket.send(JSON.stringify(message, null, 4));
+    }
+
+    /**
+     * Displays message to user and reads the value
+     * @param message
+     */
+    input(message: string): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            try {
+                this.rl.question(message, (result) => {
+                    return resolve(result);
+                });
+            } catch (exception) {
+                return reject(exception);
+            }
+        });
+    }
+}
